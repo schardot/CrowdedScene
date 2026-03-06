58b65d786167cbb2484241576f9895bb05414244
Natália Leite Schardosim <131466597+schardot@users.noreply.github.com>
2026-03-06 16:20:26 +0100

    feat: implement vertical NPC flow and allow leaving the street

--- systems/CrowdManager.gd
+++ systems/CrowdManager.gd
@@ -1,16 +1,22 @@
 extends Node2D
 class_name CrowdManager
 
-@export var crowd_count := 100
+@export var crowd_count := 10
 @onready var street: Area2D
 
 const CROWD_MEMBER_SCN: PackedScene = preload("res://scenes/entities/crowd/CrowdMember.tscn")
 
 func spawn_crowd() -> void:
 	for i in range(crowd_count):
-		var npc = CROWD_MEMBER_SCN.instantiate() as CrowdMember
-		
-		npc.global_position = street.get_random_point()
-		npc.street = street
+		spawn_npc()
 
-		add_child(npc)
+func spawn_npc():
+	var npc = CROWD_MEMBER_SCN.instantiate() as CrowdMember
+	
+	var spawn = street.get_spawn_point()
+	npc.global_position = spawn
+	
+	npc.street = street
+	npc.set_direction_from_spawn(spawn, street.get_center())
+
+	add_child(npc)
